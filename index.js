d2a43c6bb2935849c008dc4470cb8cb1e420c844
Vasil Bit <vasil.bituni@gmail.com>
2021-04-07 13:03:02 +0200

    Add ne changes

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const toggleMenu = (() => {
   menuItems.forEach((item) => {
     item.addEventListener('click', display);
   });
+
+  const menuBtn = document.querySelector('.menu');
+  const menu = document.querySelector('.main-menu-mobile');
+
+  function display() {
+    menu.classList.toggle('visible');
+  }
+
+  menuBtn.addEventListener('click', display);
 })();
 
 module.esports.toggleMenu = toggleMenu;
